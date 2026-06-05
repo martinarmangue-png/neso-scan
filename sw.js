1e73daf366643c495f149cc96e483ce90576e1e0
Martin Armangué <martinarmangue@mac.home>
2026-06-06 00:59:54 +0200

    Restore default PWA entry to home

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'neso-app-v2-20260606-2';
+const CACHE_NAME = 'neso-app-20260606-3';
 const APP_SHELL = [
   './',
   './index.html',
@@ -37,6 +37,6 @@ self.addEventListener('fetch', (event) => {
         caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
         return response;
       })
-      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./app-v2.html')))
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html')))
   );
 });
